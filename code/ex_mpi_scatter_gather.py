2abd1107ea94d7febcd68e41881ef00e26ebcc7a
Valentin Haenel <valentin.haenel@gmx.de>
2013-11-27 19:16:09 +0100

    better scatter and gather example using MPI

--- code/ex_mpi_scatter_gather.py
+++ code/ex_mpi_scatter_gather.py
@@ -4,7 +4,7 @@ import numpy
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 
-N = 4
+N = 10
 # rank 0 should send the message
 if comm.rank == 0:
     msg = numpy.arange(N, dtype=int)
@@ -16,13 +16,19 @@ dest = numpy.empty(N/comm.size, dtype=int)
 # ans should hold the gathered result
 ans = numpy.empty(comm.size, dtype=int)
 
-# scatter the array
+# scatter the array, from rank 0
 comm.Scatter([msg, MPI.INT],
              [dest, MPI.INT], root=0)
 
 # do the work
 mysum = numpy.sum(dest)
 
-# gather the result
+# gather the result, into rank 0
 comm.Gather([mysum, MPI.INT],
             [ans, MPI.INT], root=0)
+
+# output result depending on rank
+if comm.rank == 0:
+    print "Answer from rank 0: %s" % str(ans)
+else:
+    print "Answer from rank %d: %s" % (comm.rank, str(ans))
